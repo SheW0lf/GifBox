c8fcccafc774fc74cbbaaad36c6e6a4b6b13656b
Amber Cyr <seylovie@gmail.com>
2019-05-27 23:56:15 -0700

    added a loader component

---
+++ src/Components/Loader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+const Loader = () => {
+    return(
+        <div>
+            <FontAwesomeIcon icon="spinner" />
+        </div>
+    );
+}
+
+export default Loader;
